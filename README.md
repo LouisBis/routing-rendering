ad208c84fd432127432dc2c00b3f8a3ef73a0215
Louis Marais <louis-marais@hotmail.fr>
2024-09-18 11:22:17 +0200

    Client-side Data Fetching

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# routing-rendering
+# Set up
+
+Créer un fichier `.env` a la racine, insérer `NEXT_PUBLIC_API_URL='http://localhost:8080'`
+
+Dans un premier terminal
+
+```sh
+cd backend
+npm install
+npm start
+```
+
+Dans un second terminal
+
+```sh
+npm install
+npm run dev
+```
--- app/(content)/news/page.js
+++ app/(content)/news/page.js
@@ -1,11 +1,49 @@
-import { DUMMY_NEWS } from "@/dummy-news";
+"use client";
+
 import NewsList from "@/components/NewsList";
+import { useEffect, useState } from "react";
 
 export default function NewsPage() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState();
+  const [news, setNews] = useState();
+
+  useEffect(() => {
+    async function fetchNews() {
+      setIsLoading(true);
+      const response = await fetch(process.env.NEXT_PUBLIC_API_URL + "/news");
+
+      if (!response.ok) {
+        setError("Failed to fetch news.");
+        setIsLoading(false);
+      }
+
+      const data = await response.json();
+      setIsLoading(false);
+      setNews(data);
+    }
+
+    fetchNews();
+  }, []);
+
+  if (isLoading) {
+    return <p>Loading ...</p>;
+  }
+
+  if (error) {
+    return <p>{error}</p>;
+  }
+
+  let newsContent;
+
+  if (news) {
+    newsContent = <NewsList news={news} />;
+  }
+
   return (
     <>
       <h1>News Page</h1>
-      <NewsList news={DUMMY_NEWS} />
+      {newsContent}
     </>
   );
 }
